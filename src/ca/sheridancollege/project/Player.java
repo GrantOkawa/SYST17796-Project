c585a86c886f6ec89bf82996165bb0836d761dec
Daojim <jimmyddao@gmail.com>
2025-06-03 20:59:56 -0400

    Rename authors

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -8,8 +8,9 @@ package ca.sheridancollege.project;
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
+ * @author Jimmy Dao
+ * @author Grant Okawa
+ * @author Nghi Lam Vo
  */
 public abstract class Player {
 
