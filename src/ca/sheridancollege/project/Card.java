1f5d2ddcee5f42273c359e01b53a738d40ed947d
Daojim <jimmyddao@gmail.com>
2025-06-03 21:59:18 -0400

    Add card properties

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -15,6 +15,32 @@ package ca.sheridancollege.project;
  */
 public abstract class Card {
     //default modifier for child classes
+    public enum Suit{
+        HEARTS, CLUBS, SPADES, DIAMONDS
+    }
+    public enum Value {
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN,
+        EIGHT, NINE, TEN, JACK, QUEEN, KING
+    }
+
+    private Value value;
+    private Suit suit;
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
 
     /**
      * Students should implement this method for their specific children classes
