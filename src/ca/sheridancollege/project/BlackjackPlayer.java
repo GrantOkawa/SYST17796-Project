04fda0164f28e2f3f9c1cc546b6ac1bc6d344e36
maxins1211 <lamvo.general@gmail.com>
2025-06-05 15:03:30 -0400

    Create specific class for black jack player inherit from abstract Player

---
+++ src/ca/sheridancollege/project/BlackjackPlayer.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author lamvo
+ */
+public class BlackjackPlayer extends Player {
+
+    public BlackjackPlayer(String name) {
+        super(name);
+    }
+
+    @Override
+    public void play() {
+    }
+}
