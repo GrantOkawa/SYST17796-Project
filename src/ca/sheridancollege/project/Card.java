b5746cbef5778d7dfff5e4a6bc69e90b88c596da
maxins1211 <lamvo.general@gmail.com>
2025-06-05 15:04:31 -0400

    Move all properties and methods to StandardCard

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -6,8 +6,9 @@
 package ca.sheridancollege.project;
 
 /**
- * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
- * game. Students wishing to add to the code should remember to add themselves as a modifier.
+ * A class to be used as the base Card class for the project. Must be general
+ * enough to be instantiated for any Card game. Students wishing to add to the
+ * code should remember to add themselves as a modifier.
  *
  * @author Jimmy Dao
  * @author Grant Okawa
@@ -15,37 +16,12 @@ package ca.sheridancollege.project;
  */
 public abstract class Card {
     //default modifier for child classes
-    public enum Suit{
-        HEARTS, CLUBS, SPADES, DIAMONDS
-    }
-    public enum Value {
-        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN,
-        EIGHT, NINE, TEN, JACK, QUEEN, KING
-    }
-
-    private Value value;
-    private Suit suit;
-
-    public Value getValue() {
-        return value;
-    }
-
-    public void setValue(Value value) {
-        this.value = value;
-    }
-
-    public Suit getSuit() {
-        return suit;
-    }
-
-    public void setSuit(Suit suit) {
-        this.suit = suit;
-    }
 
     /**
      * Students should implement this method for their specific children classes
      *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     * @return a String representation of a card. Could be an UNO card, a
+     * regular playing card etc.
      */
     @Override
     public abstract String toString();
