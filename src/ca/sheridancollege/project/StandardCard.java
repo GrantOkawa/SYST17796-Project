bbf46c001b2121f64e1becbcc0e3562297fd66bb
Daojim <jimmyddao@gmail.com>
2025-06-07 13:59:54 -0400

    Add numerical value to card value enum

--- src/ca/sheridancollege/project/StandardCard.java
+++ src/ca/sheridancollege/project/StandardCard.java
@@ -15,8 +15,14 @@ public class StandardCard extends Card {
     }
 
     public enum Value {
-        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN,
-        EIGHT, NINE, TEN, JACK, QUEEN, KING
+        ACE(11), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7),
+        EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10);
+        
+        private final int cardValue;
+        
+        private Value(int cardValue) {this.cardValue = cardValue;}
+        
+        public int getCardValue() {return cardValue;}
     }
 
     private Value value;
